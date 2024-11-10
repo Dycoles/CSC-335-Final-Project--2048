6d5d05ccb6ffae1257105de8b543443c9ccac1a1
Dycoles <134562267+Dycoles@users.noreply.github.com>
2024-11-10 12:36:51 -0700

    Update Tile.java

--- Tile.java
+++ Tile.java
@@ -1,7 +1,40 @@
 package Components;
 
-public class Tile {
-	public Tile() {
+public class Tiles {
+	int thisValue;
+	public Tiles(int val) {
+		thisValue = val;
+	}
+	
+	public int getValue() {
+		return 0;
+	}
+	
+	public void removeValue() {
 		
+	}
+	
+	public boolean isEmpty() {
+		return false;
+	}
+	
+	public void canMove() {
+		
+	}
+	
+	public boolean canMerge() {
+		return false;
+	}
+	
+	public void moveVal() {
+		
+	}
+	
+	public void mergeVal() {
+		
+	}
+	
+	private int doubleVal(int val) {
+		return val;
 	}
 }
