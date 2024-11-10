029bfebb9676d8568a4a9be539ec2d65fb23e4d7
Dycoles <134562267+Dycoles@users.noreply.github.com>
2024-11-10 11:38:31 -0700

    Create Board.java
    Board class

---
+++ Board.java
@@ -0,0 +1,30 @@
+package Components;
+
+import java.util.ArrayList;
+
+public class Board {
+	// set global vars
+	private int thisBoardSize;
+	private Tiles tile = new Tiles();
+	private ArrayList<Tiles> board;
+	// initialize board with custom size
+	public Board(int boardSize) {
+		thisBoardSize = boardSize;
+		createBoard(thisBoardSize);
+	}
+	
+	// create the board
+	private void createBoard(int size) {
+		
+	}
+	
+	// shift tiles on the board
+	private void shiftTile() {
+		
+	}
+	
+	// get the board (which is an arrayList of tiles)
+	public ArrayList<Tiles> getBoard() {
+		return board;
+	}
+}
