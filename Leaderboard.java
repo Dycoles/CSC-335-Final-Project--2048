71034a8b7799eef241286ad237ef6eb9dad2b7d1
Dycoles <134562267+Dycoles@users.noreply.github.com>
2024-11-10 12:37:08 -0700

    Update Leaderboard.java

--- Leaderboard.java
+++ Leaderboard.java
@@ -1,5 +1,22 @@
 package Components;
 
-public class Leaderboard {
+import java.util.ArrayList;
 
+public class Leaderboard {
+	private String name;
+	private ArrayList<Integer> highScores;
+	public Leaderboard() {
+		name = "";
+		highScores = new ArrayList<Integer>();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public ArrayList<Integer> getScores() {
+		ArrayList<Integer> scores = new ArrayList<Integer>();
+		scores = highScores;
+		return scores;
+	}
 }
