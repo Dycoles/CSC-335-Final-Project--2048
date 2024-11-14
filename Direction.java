1d3f9974d3c1a277e77b33c3e2e524f4142b59ae
jackmwilliams <166460615+jackmwilliams@users.noreply.github.com>
2024-11-14 12:28:45 -0700

    Add horiDiff() and vertDiff()

--- Direction.java
+++ Direction.java
@@ -1,3 +1,24 @@
+
 public enum Direction {
-	UP, DOWN, LEFT, RIGHT
+	UP, DOWN, LEFT, RIGHT;
+	
+	// Horizontal difference after shifting in this direction:
+	public int horiDiff() {
+		if (this.equals(Direction.LEFT))
+			return -1;
+		else if (this.equals(Direction.RIGHT))
+			return 1;
+		else
+			return 0;
+	}
+	
+	// Vertical difference after shifting in this direction:
+	public int vertDiff() {
+		if (this.equals(Direction.UP))
+			return -1;
+		else if (this.equals(Direction.DOWN))
+			return 1;
+		else
+			return 0;
+	}
 }
