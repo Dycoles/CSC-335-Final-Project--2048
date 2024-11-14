1a86dd6b23113059157aa2e0529f9e4b1874babc
jackmwilliams <166460615+jackmwilliams@users.noreply.github.com>
2024-11-14 12:07:54 -0700

    Add initializeBoard()

--- Board.java
+++ Board.java
@@ -1,5 +1,7 @@
 package Components;
 
+import java.util.Random;
+
 public class Board {
 	// set global vars
 	private int thisBoardSize;
@@ -10,12 +12,29 @@ public class Board {
 	public Board(int boardSize) {
 		thisBoardSize = boardSize;
 		myScore = 0;
-		board = createBoard(thisBoardSize);
+		board = initializeBoard(thisBoardSize);
 	}
 	
 	// create the board
-	private Tile[][] createBoard(int size) {
-		return null;
+	private Tile[][] initializeBoard(int size) {
+		Random rand = new Random();
+		
+		Tile[][] newBoard = new Tile[size][size];
+		
+		// Create the initial two tile values:
+		int t1Row = rand.nextInt(size);
+		int t1Col = rand.nextInt(size);
+		newBoard[t1Row][t1Col].initialize();
+		
+		// Create the second tile value, ensuring a different one:
+		int t2Row, t2Col;
+		do {
+			t2Row = rand.nextInt(size);
+			t2Col = rand.nextInt(size);
+		} while (t2Row != t1Row && t2Col != t1Col);
+		newBoard[t2Row][t2Col].initialize();
+		
+		return newBoard;
 	}
 	
 	// shift tiles on the board
