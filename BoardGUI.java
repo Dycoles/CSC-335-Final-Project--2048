76cf4187dd03ef5e688b9dca8af32436d49a71a5
arshawissinger <166645389+arshawissinger@users.noreply.github.com>
2024-11-17 19:11:26 -0700

    Update BoardGUI.java
    create the initial GUI

--- BoardGUI.java
+++ BoardGUI.java
@@ -1,5 +1,30 @@
 package GUI;
 
-public class BoardGUI {
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+public class BoardGUI extends JFrame {
+	
+	private GameManager manager;
+	
+	public static void main(String[] args) {
+        BoardGUI gui = new BoardGUI();
+        gui.setVisible(true);
+	}
+	
+	public BoardGUI() {
+		manager = new GameManager();
+		setTitle("2048");
+		setSize(1000, 800);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
+		JPanel controlPanel = new JPanel();
+		controlPanel.setLayout(new GridLayout(1, 2));
+		controlPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		
+	}
+	
 }
