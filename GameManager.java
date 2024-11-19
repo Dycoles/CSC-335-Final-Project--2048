e4de23da6f7fb6eabb8586b4502280d300902902
jackmwilliams <166460615+jackmwilliams@users.noreply.github.com>
2024-11-19 08:56:30 -0700

    Misc. changes

--- GameManager.java
+++ GameManager.java
@@ -3,33 +3,36 @@ package mvc;
 import Components.Board;
 import Components.Direction;
 import Components.Leaderboard;
-import java.util.ArrayList;
 
+import java.util.ArrayList;
 
 public class GameManager {
 	
 	private Board board;
-	private Leaderboard leaderboard;
 	private Boolean isPlaying;
-	//private int currScore;
+	private int curScore;
 	//private int thisBoardSize;
 
 	public GameManager(int boardSize) {
 		this.board = new Board(boardSize);
-		this.leaderboard = new Leaderboard();
 		isPlaying = true;
+		curScore = 0;
 	}
 	
 	// shift tiles on the board
 	public void shift(Direction dir) {
 		if (dir == Direction.UP) {
-			board.shiftTileUp();
+			curScore += board.shiftUp();
 		} else if (dir == Direction.RIGHT) {
-			board.shiftTileRight();
+			curScore += board.shiftRight();
 		} else if (dir == Direction.LEFT) {
-			board.shiftTileLeft();
+			curScore += board.shiftLeft();
 		} else if (dir == Direction.DOWN) {
-			board.shiftTileDown();
+			curScore += board.shiftDown();
 		}
+	}
+	
+	public int getCurScore() {
+		return curScore;
 	}
 }
