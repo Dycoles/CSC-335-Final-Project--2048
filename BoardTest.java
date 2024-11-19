88d16d16e6f19f6ff494bc6e1c83c3bbbed64257
arshawissinger <166645389+arshawissinger@users.noreply.github.com>
2024-11-19 16:59:46 -0700

    Update BoardTest.java
    added testTile

--- BoardTest.java
+++ BoardTest.java
@@ -6,6 +6,24 @@ public class BoardTest {
 
 	@Test
 	public void testTile() {
+		Tile tile = new Tile();
+		assertEquals(tile.getValue(), 0);
+		
+		Tile twoTile = new Tile(2);
+		assertEquals(twoTile.getValue(), 2);
+		
+		twoTile.removeValue();
+		assertEquals(twoTile.getValue(), 0);
+
+		assertEquals(twoTile.isEmpty(), true);
+		
+		// testing canMove/canMerge
+		assertEquals(tile.canMove(twoTile), true);
+		assertEquals(tile.canMerge(twoTile), true);
+	}
+	
+	@Test
+	public void testBoard() {
 		Board board = new Board(4);
 		assertEquals(board.gameWon(), false);
 		assertEquals(board.gameLost(), false);
