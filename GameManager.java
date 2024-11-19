11e629110db16269b1b1486373da23a4ebdaaa9a
jackmwilliams <166460615+jackmwilliams@users.noreply.github.com>
2024-11-19 08:34:07 -0700

    Basic functionality

--- GameManager.java
+++ GameManager.java
@@ -2,4 +2,22 @@ package mvc;
 
 public class GameManager {
 	
+	private Board board;
+
+	public GameManager(int boardSize) {
+		board = new Board(boardSize);
+	}
+	
+	// shift tiles on the board
+	public void shift(Direction dir) {
+		if (dir == Direction.UP) {
+			board.shiftTileUp();
+		} else if (dir == Direction.RIGHT) {
+			board.shiftTileRight();
+		} else if (dir == Direction.LEFT) {
+			board.shiftTileLeft();
+		} else if (dir == Direction.DOWN) {
+			board.shiftTileDown();
+		}
+	}
 }
