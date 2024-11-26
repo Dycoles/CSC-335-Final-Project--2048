11f0575557fe97468af91a75a7001647e0da1500
s-farlow <166459503+s-farlow@users.noreply.github.com>
2024-11-26 08:28:36 -0700

    Create setUp()

--- BoardGUI.java
+++ BoardGUI.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 public class BoardGUI extends JFrame {
 	
 	private GameManager manager;
+	private JPanel titlePanel;
 	
 	public static void main(String[] args) {
         BoardGUI gui = new BoardGUI();
@@ -15,17 +16,23 @@ public class BoardGUI extends JFrame {
 	}
 	
 	public BoardGUI() {		
-		manager = new GameManager();
+		manager = new GameManager(4);
+		setUp();
 		
+	}
+	
+	public void setUp() {
 		setTitle("2048");
 		setSize(1000, 800);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
 		
+		// set up the title label
 		JLabel title = new JLabel("2048", JLabel.CENTER);
 		title.setSize(1000, 100);
 		this.add(title);
 		
+		// 
 		JPanel controlPanel = new JPanel();
 		controlPanel.setLayout(new GridLayout(1, 2));
 		controlPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
