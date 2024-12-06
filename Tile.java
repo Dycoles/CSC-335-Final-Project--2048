63fbf85af55f3be30bfe88c089517707e202d332
s-farlow <166459503+s-farlow@users.noreply.github.com>
2024-12-06 08:52:56 -0700

    Add documentation

--- Tile.java
+++ Tile.java
@@ -1,5 +1,6 @@
 /**
- * Tile.java will build each tile that will be used in the game.
+ * Tile.java represents an individual tile on the 2048 board. It will carry
+ * out movement and merge operations and also manage the value of the tile.
  * 
  * @author Dylan Coles (NetID: colesdylan12)
  * @author Sydney Farlow (NetID; sfarlow)
@@ -10,46 +11,83 @@
 import java.util.Random;
 
 public class Tile {
+	
 	private int value;
 	
+	/**
+	 * Create tile and set initial tile value to 0.
+	 */
 	public Tile() {
 		value = 0;
 	}
 	
+	/**
+	 * Create tile and set initial value to specified val
+	 * @param val - initial value of the tile
+	 */
 	public Tile(int val) {
 		value = val;
 	}
 	
+	/**
+	 * Initializes the tile with 2 or 4 with a 7/10 chance for a 2
+	 */
 	public void initialize() {
 		// Assign either 2 or 4 to this tile:
 		Random rand = new Random();
 		int valueChance = rand.nextInt(10);
-		if (valueChance <= 6)	// 7/10 chance for a 2 TODO play with probability
+		// 7/10 chance for a 2
+		if (valueChance <= 6) 
 			this.setVal(2);
 		else
 			this.setVal(4);
 	}
 	
+	/**
+	 * Getter for value.
+	 * @return value - value of the tile
+	 */
 	public int getValue() {
 		return value;
 	}
 	
+	/**
+	 * Removes value of the tile.
+	 */
 	public void removeValue() {
 		value = 0;
 	}
 	
+	/**
+	 * Checks if the tile is empty
+	 * @return true if value is 0, otherwise false
+	 */
 	public boolean isEmpty() {
 		return value == 0;
 	}
 	
+	/**
+	 * Checks if the tile is able to make a move to an empty spot
+	 * @param to - the tile in the desired direction
+	 * @return True if spot is empty, otherwise false
+	 */
 	public boolean canMove(Tile to) {
 		return to.isEmpty();
 	}
 	
+	/**
+	 * Checks if the tile is able to merge with another tile
+	 * @param to - the tile in the desired direction
+	 * @return True if both tiles have the same value, otherwise false
+	 */
 	public boolean canMerge(Tile to) {
 		return to.getValue() == this.getValue();
 	}
 	
+	/**
+	 * Moves the tile value to other tile
+	 * @param to - the tile spot in the desired direction
+	 */
 	public void moveVal(Tile to) {
 		if (to != this) {
 			to.setVal(value);
@@ -57,6 +95,11 @@ public class Tile {
 		}
 	}
 	
+	/**
+	 * Merges the tile value with another tile
+	 * @param to - the tile spot in the desired direction
+	 * @return value after the merge, 0 if merge cannot be done
+	 */
 	public int mergeVal(Tile to) {
 		if (to != this) {
 			to.doubleVal();
@@ -66,10 +109,17 @@ public class Tile {
 		return 0;
 	}
 	
+	/**
+	 * Setter for the tile's value
+	 * @param val - the tile's value
+	 */
 	private void setVal(int val) {
 		value = val;
 	}
 	
+	/**
+	 * Checks double value 
+	 */
 	private void doubleVal() {
 		value = value << 1;
 	}
