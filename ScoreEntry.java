aef3946f52107ada6ee3237dceb466a4c21008cb
arshawissinger <166645389+arshawissinger@users.noreply.github.com>
2024-12-06 09:31:42 -0700

    Update ScoreEntry.java

--- ScoreEntry.java
+++ ScoreEntry.java
@@ -12,25 +12,47 @@ public class ScoreEntry {
 	private String name;
 	private int score;
 	private int size;
-	
+
+	/**
+	* Initialize ScoreEntry with name, score, and size
+	* @param name - name of player
+  	* @param score - score of player
+   	* @param size - custom board size
+	*/
 	public ScoreEntry(String name, int score, int size) {
 		this.name = name;
 		this.score = score;
 		this.size = size;
 	}
-	
+
+	/**
+	* return this.name, the name of the player
+	* @return name - name of player
+	*/
 	public String getName() {
 		return name;
 	}
-	
+
+	/**
+	* return this.score, the score of the player
+	* @return score - the score of the player
+	*/
 	public int getScore() {
 		return score;
 	}
-	
+
+	/**
+	* return this.size, the size of the board
+	* @return size - the size of the board
+	*/
 	public int getSize() {
 		return size;
 	}
 
+	/**
+	* returns string representation of object. (for testing)
+	* @return name + " " + score + " " + size, name of player + score of player + size of board
+	*/
 	public String toString() {
 		return name + " " + score + " " + size;
 	}
