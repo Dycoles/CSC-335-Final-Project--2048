062ef94d5b0f98c2440868aac6c98cf3492e1d9d
arshawissinger <166645389+arshawissinger@users.noreply.github.com>
2024-12-06 09:23:55 -0700

    Update Composite2048Observer.java
    doccumentation

--- Composite2048Observer.java
+++ Composite2048Observer.java
@@ -1,3 +1,13 @@
+
+/**
+ * Composite2048Observer.java is the observer class
+ * we are implementing in our GUI.
+ * 
+ * @author Dylan Coles (NetID: colesdylan12)
+ * @author Sydney Farlow (NetID; sfarlow)
+ * @author Jack Williams (NetID: jackmwilliams)
+ * @author Arsha Wissinger (NetID: arshawissinger)
+ */
 public interface Composite2048Observer {
 	public void updateObserver(int row, int col, int value);
 }
