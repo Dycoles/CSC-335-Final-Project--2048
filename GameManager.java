8601b93bb68b4c9b3fd5001a2cd17e4fe7845a31
jackmwilliams <166460615+jackmwilliams@users.noreply.github.com>
2024-12-06 22:17:42 -0700

    Final version of GameManager

--- GameManager.java
+++ GameManager.java
@@ -8,113 +8,108 @@
  * @author Jack Williams (NetID: jackmwilliams)
  * @author Arsha Wissinger (NetID: arshawissinger)
  */
+
 import java.util.ArrayList;
 
 public class GameManager {
-	
+
 	private Board board;
-	private Boolean isPlaying;
 	private Leaderboard leaderboard;
 
 	/**
 	 * Creates new game with a given board size.
-	 * @param boardSize - the size of the board
+	 * 
+	 * @param boardSize - the size of the board.
 	 */
 	public GameManager(int boardSize) {
 		this.board = new Board(boardSize);
 		this.leaderboard = new Leaderboard();
-		isPlaying = true;
-	}
-	
-	/**
-	 * Checks if the game has been won
-	 * @return returns true if a tile is 2048, otherwise false
-	 */
-	public boolean isGameWon() {
-		if (board.gameWon()) {
-			isPlaying = false;
-		}
-		return board.gameWon();
-	}
-	
-	/**
-	 * Checks if game has been lost
-	 * @return returns true if no more moves, otherwise false
-	 */
-	public boolean isGameLost() {
-		if (board.gameLost()) {
-			isPlaying = false;
-		}
-		return board.gameLost();
-	}
-	
-	/**
-	 * Checks if the game is still active
-	 * @return true if the game is still active, otherwise false
-	 */
-	public boolean isPlaying() {
-		return isPlaying;
 	}
 
 	/**
-	 * Print current game board
+	 * Checks if the game has been won.
+	 * 
+	 * @return returns true if a tile is 2048, otherwise false.
+	 */
+	public boolean isGameWon() {
+		return board.gameWon();
+	}
+
+	/**
+	 * Checks if game has been lost.
+	 * 
+	 * @return returns true if there are no more moves, otherwise false.
+	 */
+	public boolean isGameLost() {
+		return board.gameLost();
+	}
+
+	/**
+	 * Checks if the game is still active.
+	 * 
+	 * @return true if the game is still active, otherwise false.
+	 */
+	public boolean isPlaying() {
+		return !board.gameWon() && !board.gameLost();
+	}
+
+	/**
+	 * Print the current game board to the console.
 	 */
 	public void printCurrentBoard() {
 		board.printBoard();
 	}
-	
+
 	/**
-	 * Shift tiles on the board
-	 * @param dir - the direction the tile will shift
+	 * Shift all tiles on the board.
+	 * 
+	 * @param dir - the direction the tile will shift in.
 	 */
 	public void shift(Direction dir) {
-		// check for valid game
-		if (!isPlaying) {
-			return;
-		}
-		// shift tile
-		board.shiftTile(dir);
-		// checks if game is over, sets isPlaying
-		if (isGameWon() || isGameLost()) {
-			isPlaying = false;
-			return;
+		// Check to see if this game is still ongoing, shifting if so:
+		if (this.isPlaying()) {
+			board.shiftTile(dir);
 		}
 	}
-	
+
 	/**
-	 * Gets current score
-	 * @return current score if the game
+	 * Get the current score.
+	 * 
+	 * @return the current score of the game.
 	 */
 	public int getCurScore() {
 		return board.getScore();
 	}
-	
+
 	/**
-	 * Add a composite observer to the model:
-	 * @param o - observer
+	 * Add a composite observer to the model.
+	 * 
+	 * @param o - the observer to add.
 	 */
 	public void addCompositeObserver(Composite2048Observer o) {
 		board.addCompositeObserver(o);
 	}
-	
+
 	/**
-	 * Loads the leaderboard
+	 * Loads the leaderboard from the Leaderboard.txt file.
 	 */
 	public void loadLeaderboard() {
 		leaderboard.loadScores();
 	}
-	
+
 	/**
-	 * Getter for leaderboard list
+	 * Getter for leaderboard list.
+	 * 
 	 * @return an ArrayList of scores.
 	 */
 	public ArrayList<ScoreEntry> getLeaderboard() {
 		return leaderboard.getScoreList();
 	}
-	
+
 	/**
-	 * Adds name to the leaderboard with corresponding score
-	 * @param name - the name of the player
+	 * Adds a name to the leaderboard with their corresponding score.
+	 * 
+	 * @param name - the name of the player.
 	 */
 	public void addScoreToLeaderboard(String name) {
 		leaderboard.addScore(name, getCurScore(), board.getSize());
