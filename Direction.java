bbcd84d8c992acfe3be0877dadb56b139ca57f75
jackmwilliams <166460615+jackmwilliams@users.noreply.github.com>
2024-12-06 22:18:15 -0700

    Final version of Direction

--- Direction.java
+++ Direction.java
@@ -1,6 +1,6 @@
 
 /**
- * Direction.java is an Enumeration class to help implement the tile shift
+ * Direction.java is an enumerated type to help implement the tile shift
  * directions. This class adds direction constants for the 2048 game.
  * 
  * @author Dylan Coles (NetID: colesdylan12)
@@ -12,15 +12,16 @@
 public enum Direction {
 	// Directions:
 	UP, DOWN, LEFT, RIGHT;
-	
+
 	/**
-	 * Return the Direction corresponding to the accepted String:
-	 * @param str - the String to convert 
-	 * @return Direction enum value that corresponds to the String, otherwise null
+	 * Return the Direction corresponding to the accepted String.
+	 * 
+	 * @param str - the String to convert.
+	 * @return the Direction value that corresponds to the String, otherwise null.
 	 */
 	public static Direction strToDir(String str) {
 		str = str.toLowerCase();
-		
+
 		// Compare possible values:
 		switch (str) {
 			case "up":
